77af18165585dbddb79fe308c0fb85f712220eb1
jiangtao <jiangtao69039@gmail.com>
2025-12-31 10:26:04 +0800

    load lib

--- src/main/java/com/github/ttttz/pgParser/PgQueryLibInterface.java
+++ src/main/java/com/github/ttttz/pgParser/PgQueryLibInterface.java
@@ -6,13 +6,43 @@ import com.github.ttttz.pgParser.split.PgQuerySplitResult;
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Objects;
 
 public interface PgQueryLibInterface extends Library {
 
-    String libPath = Objects.requireNonNull(PgQueryLibInterface.class.getClassLoader().getResource("libpg_query.so")).getFile();
+    String libPath = extractNativeLibrary();
 
     PgQueryLibInterface INSTANCE = Native.load(libPath, PgQueryLibInterface.class);
+
+    /**
+     * Extract native library from JAR to temp file.
+     */
+    static String extractNativeLibrary() {
+        String libName = "libpg_query.so";
+        try (InputStream is = PgQueryLibInterface.class.getClassLoader().getResourceAsStream(libName)) {
+            Objects.requireNonNull(is, "Native library not found: " + libName);
+
+            File tempFile = File.createTempFile("libpg_query", ".so");
+            tempFile.deleteOnExit();
+
+            try (OutputStream os = new FileOutputStream(tempFile)) {
+                byte[] buffer = new byte[8192];
+                int bytesRead;
+                while ((bytesRead = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, bytesRead);
+                }
+            }
+
+            return tempFile.getAbsolutePath();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to extract native library: " + libName, e);
+        }
+    }
 
     // ============== Split functions ==============
     PgQuerySplitResult.ByValue pg_query_split_with_scanner(String input);
